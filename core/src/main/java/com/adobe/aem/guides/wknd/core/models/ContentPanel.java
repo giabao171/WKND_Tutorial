63383f3ee937e149ae93d3e04cc81ce1aa08e41c
giabaolaptrinh <giabaon171@gmail.com>
2023-08-28 23:10:40 +0700

    content panel p1

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/ContentPanel.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.adobe.aem.guides.wknd.core.models;
+
+import java.util.Map;
+
+import com.adobe.cq.wcm.core.components.models.Component;
+import com.adobe.cq.wcm.core.components.models.Image;
+
+/**
+ * @author USER
+ *
+ */
+public interface ContentPanel extends Component{
+
+	Image getImage();
+	
+	String getTitle();
+	
+	String getDescription();
+	
+	String getLink();
+	
+	boolean isOpenNewTab();
+	
+	String getStyleSystemClasses(Map<String, String> styleSystemMap);
+	
+	boolean isAutoPlay();
+	
+	boolean hideControlBar();
+}
---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/ContentPanelImpl.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package com.adobe.aem.guides.wknd.core.models.impl;
+
+import java.util.Map;
+
+import com.adobe.aem.guides.wknd.core.models.ContentPanel;
+import com.adobe.cq.wcm.core.components.models.Image;
+
+/**
+ * @author USER
+ *
+ */
+public class ContentPanelImpl implements ContentPanel{
+
+	@Override
+	public Image getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLink() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isOpenNewTab() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getStyleSystemClasses(Map<String, String> styleSystemMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAutoPlay() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hideControlBar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
